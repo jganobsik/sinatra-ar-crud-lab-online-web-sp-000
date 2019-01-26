e43bad9047b320fc953b7a2c7feb6544183e5060
Joe Ganobsik <jganobsik@gmail.com>
2019-01-26 16:20:10 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,15 @@
 
 class Article < ActiveRecord::Base
   attr_accessor :title, :content
+  @@all = []
+  def initialize(title, content)
+    @title = title
+    @content = content
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
   
 end
